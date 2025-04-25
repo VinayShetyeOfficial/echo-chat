b904b492362e83850b95a0852cd537f525221f09
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-04-24 23:25:50 -0400

    updated

--- CLIENT/src/pages/InvitePage.tsx
+++ CLIENT/src/pages/InvitePage.tsx
@@ -1,11 +1,9 @@
-import React, { useEffect, useState } from "react";
+"use client";
+
+import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { useAuth } from "@/contexts/AuthContext";
-import {
-  verifyInvitation,
-  redeemInvitation,
-  getPublicInviteDetails,
-} from "@/lib/api";
+import { redeemInvitation, getPublicInviteDetails } from "@/lib/api";
 import { Button } from "@/components/ui/button";
 import {
   Card,
