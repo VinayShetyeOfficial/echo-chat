34ed9bbbba8ae2d0cde62dbfc89bf080f11b56f8
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-05-03 22:37:34 -0400

    updated

--- CLIENT/src/pages/_app.tsx
+++ CLIENT/src/pages/_app.tsx
@@ -1,8 +1,8 @@
-import { AppProps } from "next/app";
-import { SessionProvider } from "next-auth/react";
-import { AuthProvider } from "@/contexts/AuthContext";
-import { SettingsProvider } from "@/contexts/SettingsContext";
-import "@/styles/globals.css";
+import type { AppProps } from "next/app"
+import { SessionProvider } from "next-auth/react"
+import { AuthProvider } from "@/contexts/AuthContext"
+import { SettingsProvider } from "@/contexts/SettingsContext"
+import "@/styles/globals.css"
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
@@ -13,7 +13,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
         </AuthProvider>
       </SettingsProvider>
     </SessionProvider>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
