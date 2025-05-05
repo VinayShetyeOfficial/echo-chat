db20f78c9be63f9004eca0b148a487aa9625c2df
VinayShetyeOfficial <vinay.courses.official@gmail.com>
2025-05-04 22:26:24 -0400

    updated

---
+++ CLIENT/src/routes/users.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import {
+  getMe,
+  updateProfile,
+  getAllUsers,
+  getUserById,
+} from "../controllers/users";
+import { protect } from "../middleware/auth";
+import { createHandler } from "../utils/routeHandler";
+
+const router = express.Router();
+
+// All routes in this router require authentication
+router.use(protect as express.RequestHandler);
+
+// User routes
+router.get("/me", createHandler(getMe));
+router.put("/me", createHandler(updateProfile));
+router.get("/", createHandler(getAllUsers));
+router.get("/:id", createHandler(getUserById));
+
+export default router;
